ccbf7b0dcf8352dba36333cd01c6e6654061ce04
Peter Parente <parente@cs.unc.edu>
2010-09-11 21:27:48 -0400

    Add refresh button

--- servers/common/uow/ui/nls/CollectionEditor.js
+++ servers/common/uow/ui/nls/CollectionEditor.js
@@ -7,6 +7,8 @@
     undo_button_title : 'Undo the last change',
     redo_button_label : 'Redo',
     redo_button_title : 'Redo the last change',
+    refresh_button_label : 'Refresh',
+    refresh_button_title : 'Refresh the grid view',
     schema_error_label : 'Collection does not have a schema',
     db_error_label : 'Could not connect to database'
 }
