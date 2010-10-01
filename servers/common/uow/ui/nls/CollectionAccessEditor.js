b33e13c24760054a355377cf85960cd6334b9b96
Peter Parente <pparent@us.ibm.com>
2010-09-30 22:30:12 -0400

    nls strings for CollectionAccessEditor

--- servers/common/uow/ui/nls/CollectionAccessEditor.js
+++ servers/common/uow/ui/nls/CollectionAccessEditor.js
@@ -1,3 +1,12 @@
 {
-    
+    create_column_label : 'Create (c)',
+	read_column_label : 'Read (r)'
+	update_column_label : 'Update (u)',
+	delete_column_label : 'Delete (d)',
+	restricted_read_column_label : 'Restricted read (R)',
+	anonymous_row_label : 'anonymous',
+	identified_row_label : 'identified',
+	author_row_label : 'author',
+	admin_row_label : 'admin',
+	loading_label : 'Loading...'
 }
